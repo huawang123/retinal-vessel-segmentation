57e21f9ae46dad384cf5471159658225bce572fc
hwang0609 <752448068@qq.com>
2020-05-21 16:06:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import argparse
 import sys
 import os
 # 当前项目路径加入到环境变量中，让解析器能找到第一model的目录
-father_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+father_dir = os.path.abspath(os.path.join(os.getcwd(), ""))
 sys.path.append(father_dir)
 
 from srcipt.SUnet import small_UNet
@@ -46,9 +46,9 @@ def main(argv):
     parser.add_argument('--early_stopping_max_checks', type=int, default=100,
                         help='Max checks without improvement for early stopping')
 
-    parser.add_argument('--train', action='store_true', default=True,
+    parser.add_argument('--train', action='bool', default=True,
                         help='Set to True to train network')
-    parser.add_argument('--Restore', action='store_true', default=True,
+    parser.add_argument('--Restore', action='bool', default=True,
                         help='Set to True to continue train network')
     parser.add_argument('--load_checkpoint', type=str,
                         default='/home/wanghua/wh/vessel/log_c/small_UNet_20/model/model.ckpt-52',
